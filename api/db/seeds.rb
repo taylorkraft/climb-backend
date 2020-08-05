e90c54e436799e2469057e449edef1ce3305d0f8
taylorkraft <taylorkraft16@gmail.com>
2020-08-05 17:37:12 -0600

    seeds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,9 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Mountain.delete_all
 
 User.create(name: "Taylor")
 User.create(name: "Kayla")
 User.create(name:"Kaitlin")
 User.create(name: "Chris")
 User.create(name: "Joe")
+
+Mountain.create(name: "Mt. Elbert", elevation: "14,433", user_id: 8)
+Mountain.create(name: "Mt. Massive", elevation: "14,421", user_id: 8)
+Mountain.create(name: "Mt.Harvard", elevation: "14,420", user_id: 7)
+Mountain.create(name: "Blanca Peak", elevation: "14,345", user_id: 10)
+Mountain.create(name: "La Plata Peak", elevation: "14,336", user_id: 6)
